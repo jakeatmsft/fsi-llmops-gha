f00fc5be0044d609c07e4a44ab1d5597cb7db8e8
jakeatmsft <jacwang@microsoft.com>
2023-10-25 17:14:52 +0000

    updated flows

--- promptflow/evaluation/extract-accuracy-eval/grade.py
+++ promptflow/evaluation/extract-accuracy-eval/grade.py
@@ -1,7 +1,19 @@
 from promptflow import tool
+import json
 
 
 @tool
 def grade(groundtruth: str, prediction: str):
+    # groundtruth and prediction are both json objects in string format, try to convert them to json objects and compare the number of items in the complaint array
+    try:
+        groundtruth = json.loads(groundtruth)
+        prediction = json.loads(prediction)
+        if len(groundtruth['complaints']) == len(prediction['complaints']):
+            return "Correct"
+        else:
+            return "Incorrect"
+    except:
+        return "Incorrect"
     
-    return "Correct" if groundtruth.lower() == prediction.lower() else "Incorrect"
+
+
